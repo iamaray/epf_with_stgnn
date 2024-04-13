483dbab6b84dd8a26a2d79a0fb3712a2f05bfdc1
iamaray <arya.nahri@gmail.com>
2024-04-13 19:42:35 -0400

    stuff

---
+++ models/stgnn/model.py
@@ -0,0 +1,59 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
+import pandas as pd
+from torch.autograd import Variable
+import math
+device = torch.device("cuda:5" if torch.cuda.is_available() else "cpu")
+
+
+class Transform(nn.Module):
+    def __init__(self):
+        super(Transform, self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class PositionalEncoding(nn.Module):
+    def __init__(self):
+        super(PositionalEncoding, self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class SGNN(nn.Module):
+    def __init__(self):
+        super(SGNN, self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class GRU(nn.Module):
+    def __init__(self):
+        super(GRU, self).__init__()
+        pass
+
+
+class STGNNwithGRU(nn.Module):
+    def __init__(self, outfea):
+        super(STGNNwithGRU, self).__init__()
+        pass
+
+    def forward(self):
+        pass
+
+
+class STGNN(nn.Module):
+    def __init__(self, infea, outfea, L, d):
+        super(STGNN, self).__init__()
+        pass
+
+    def forward(self):
+        pass
---
+++ models/stgnn/train.py
@@ -0,0 +1,30 @@
+from tqdm import tqdm
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import time
+import argparse
+import math
+
+from model import STGNN
+
+
+def getErrorLists():
+    pass
+
+
+def train():
+    pass
+
+
+def test():
+    pass
+
+
+def compute_loss():
+    pass
+
+
+def computeMetrics():
+    pass
