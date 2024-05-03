fb7a51188c4cb12d1e8b8521dc6a0eb5b102173a
iamaray <arya.nahri@gmail.com>
2024-05-03 13:29:41 -0400

    cleaning up a little

--- models/stgnn/model.py
+++ models/stgnn/model.py
@@ -8,49 +8,6 @@ import math
 import modules
 device = torch.device("cuda:5" if torch.cuda.is_available() else "cpu")
 
-
-# class Transform(nn.Module):
-#     def __init__(self):
-#         super(Transform, self).__init__()
-#         pass
-
-#     def forward(self):
-#         pass
-
-
-# class PositionalEncoding(nn.Module):
-#     def __init__(self):
-#         super(PositionalEncoding, self).__init__()
-#         pass
-
-#     def forward(self):
-#         pass
-
-
-# class SGNN(nn.Module):
-#     def __init__(self):
-#         super(SGNN, self).__init__()
-#         pass
-
-#     def forward(self):
-#         pass
-
-
-# class GRU(nn.Module):
-#     def __init__(self):
-#         super(GRU, self).__init__()
-#         pass
-
-
-# class STGNNwithGRU(nn.Module):
-#     def __init__(self, outfea):
-#         super(STGNNwithGRU, self).__init__()
-#         pass
-
-#     def forward(self):
-#         pass
-
-
 class STGNN(nn.Module):
     def __init__(self, num_layers, num_nodes, in_features, conv_channels, residual_channels, out_features, num_hops, dilation):
         super(STGNN, self).__init__()
