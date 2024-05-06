a1c1fc3d9b68404d0584f1dc67903b7cffdd5698
iamaray <arya.nahri@gmail.com>
2024-05-05 20:08:17 -0400

    gc module implemented

--- models/stgnn/modules.py
+++ models/stgnn/modules.py
@@ -1,28 +1,78 @@
+from __future__ import division
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 import pandas as pd
 from torch.autograd import Variable
-from __future__ import division
 from torch.nn import init
 import numbers
+import os
+import sys
+# from utils.general_utils import compute_a_tilde
 
-from utils.general_utils import compute_a_tilde
+# sys.path.insert(0, os.path.abspath(".."))
+import utils.general_utils as gutils
+
+
+class SingleMixHop(nn.Module):
+    def __init__(self, K, beta, adj):
+        super(SingleMixHop, self).__init__()
+        self.K = K
+        self.beta = beta
+        self.adj = gutils.compute_a_tilde(adj)
+        self.W = self._init_weights()
+        self._init_weights()
+
+    def _init_weights(self):
+        weights = []
+        for k in range(self.K):
+            w_k = nn.Parameter(torch.randn(data.x.shape[0], data.x.shape[1]))
+            w_k.requires_grad = True
+            weights.append(w_k)
+        return torch.stack(weights)
+
+    def _prop_step(self, past_hops):
+        h_in = past_hops[-1]  # output of last pass
+        h_0 = h_in
+        curr_hops = [h_0]
+        for k in range(1, self.K):
+            curr_hops.append(
+                torch.Tensor(self.beta * h_in + (1 - self.beta) * curr_hops[k-1]))
+
+        return torch.stack(curr_hops)
+
+    def _selection_step(self, curr_hops):
+        h_out = torch.einsum('bik,bik->bik', curr_hops, self.W).sum(dim=0)
+        return h_out
+
+    def forward(self, past_hops):
+        curr_hops = self._prop_step(past_hops)
+        # curr_hops = torch.Tensor(curr_hops)
+        h_out = self._selection_step(curr_hops)
+
+        return h_out
 
 
 class GraphConvolution(nn.Module):
     """
-        The Graph Convolution (GC) module is composed of two mix-hop propagation 
-        layers, one taking A and the other taking A^T. A mix-hop propagation layer 
+        The Graph Convolution (GC) module is composed of two mix-hop propagation
+        layers, one taking A and the other taking A^T. A mix-hop propagation layer
         performs a horizontal mix-hop operation to compute the successive hidden features
         H^(k). It then feeds each of these into an MLP. The resultant output is then
         aggregated via a weighted sum.
     """
 
-    def __init__(self):
+    def __init__(self, K, beta, adj):
         super(GraphConvolution, self).__init__()
-        pass
+
+        self.mh_1 = SingleMixHop(K, beta, adj)
+        self.mh_2 = SingleMixHop(K, beta, torch.transpose(adj, 0, 1))
+
+    def forward(self, past_hops):
+        out_1 = self.mh_1(past_hops)
+        out_2 = self.mh_2(past_hops)
+        return torch.stack([out_1, out_2]).sum(dim=0)
 
 
 class TemporalConvolution(nn.Module):
@@ -54,23 +104,25 @@ class TemporalConvolution(nn.Module):
         return x
 
 
-
-
 class Output(nn.Module):
     """
-        Details needed.
+        A class that performs a 1D convolution to transform the input tensor.
     """
-    def __init__(self, num_nodes, in_features, out_features):
+
+    def __init__(self, in_channels, out_channels):
         super(Output, self).__init__()
-        pass
-        self.num_nodes = num_nodes
-        self.in_features = in_features
-        self.out_features = out_features
-        self.fc = nn.Linear(num_nodes * in_features, out_features)
+        # Use the provided input and output dimensions
+        self.conv1 = nn.Conv1d(in_channels=in_channels,
+                               out_channels=out_channels, kernel_size=1)
 
     def forward(self, x):
-        x = x.view(x.size(0), -1)  # Flatten the features across nodes
-        return self.fc(x)
+        # [batch_size, in_channels, length] -> [batch_size, length, in_channels]
+        x = x.permute(2, 1)
+        x = self.conv1(x)
+        # [batch_size, new_length, in_channels] -> [batch_size, in_channels, new_length]
+        x = x.permute(2, 1)
+
+        return x
 
 
 class LayerNorm(nn.Module):  # performs layer normalization
